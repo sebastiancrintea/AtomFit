cc56693f9ae3fc2ada76b19a6f48b826b72a13bf
Sebastian <seby.danyel@gmail.com>
2024-08-18 18:18:09 +0300

    refact(create-workout): changed logic and visual

--- src/components/shared/workout/create-workout-form.tsx
+++ src/components/shared/workout/create-workout-form.tsx
@@ -31,6 +31,15 @@ import {
   createWorkoutSchema,
   createWorkoutType,
 } from "@/schemas/create-workout-schema";
+import { FaClock } from "react-icons/fa";
+import { TiArrowRepeatOutline } from "react-icons/ti";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "@/components/ui/tooltip";
+import { useEffect } from "react";
 
 const exercises = [
   {
@@ -66,50 +75,92 @@ const exercises = [
     muscles: ["chest", "traps"],
   },
   {
-    id: 4,
-    name: "thnig",
-    description: "sanjkdbjadjaw",
-    video_url: "sanjkdawhdwa",
+    id: 6,
+    name: "Machine Learning Basics",
+    description:
+      "An introductory video on the key concepts of machine learning and how they are applied.",
+    video_url: "https://example.com/videos/machine-learning-basics",
     type: "repeats",
     muscles: ["chest", "traps"],
   },
   {
-    id: 4,
-    name: "thnig",
-    description: "sanjkdbjadjaw",
-    video_url: "sanjkdawhdwa",
+    id: 7,
+    name: "Responsive Web Design",
+    description:
+      "Learn how to create websites that work on all devices using responsive design techniques.",
+    video_url: "https://example.com/videos/responsive-web-design",
     type: "repeats",
     muscles: ["chest", "traps"],
   },
   {
-    id: 4,
-    name: "thnig",
-    description: "sanjkdbjadjaw",
-    video_url: "sanjkdawhdwa",
+    id: 8,
+    name: "Version Control with Git",
+    description:
+      "A guide to using Git for version control, covering the basics and advanced features.",
+    video_url: "https://example.com/videos/version-control-with-git",
     type: "repeats",
     muscles: ["chest", "traps"],
   },
   {
-    id: 4,
-    name: "thnig",
-    description: "sanjkdbjadjaw",
-    video_url: "sanjkdawhdwa",
+    id: 9,
+    name: "Introduction to Kubernetes",
+    description:
+      "Learn the basics of Kubernetes and how to deploy and manage containerized applications.",
+    video_url: "https://example.com/videos/introduction-to-kubernetes",
     type: "repeats",
     muscles: ["chest", "traps"],
   },
   {
-    id: 4,
-    name: "thnig",
-    description: "sanjkdbjadjaw",
-    video_url: "sanjkdawhdwa",
+    id: 10,
+    name: "SQL for Beginners",
+    description:
+      "An introductory video on SQL, the standard language for managing and querying databases.",
+    video_url: "https://example.com/videos/sql-for-beginners",
     type: "repeats",
     muscles: ["chest", "traps"],
   },
   {
-    id: 4,
-    name: "thnig",
-    description: "sanjkdbjadjaw",
-    video_url: "sanjkdawhdwa",
+    id: 11,
+    name: "Introduction to Blockchain",
+    description:
+      "A beginner's guide to understanding the basics of blockchain technology and its applications.",
+    video_url: "https://example.com/videos/introduction-to-blockchain",
+    type: "repeats",
+    muscles: ["chest", "traps"],
+  },
+  {
+    id: 12,
+    name: "Building Web Apps with React",
+    description:
+      "Learn how to build dynamic web applications using the React JavaScript library.",
+    video_url: "https://example.com/videos/building-web-apps-with-react",
+    type: "repeats",
+    muscles: ["chest", "traps"],
+  },
+  {
+    id: 13,
+    name: "Cybersecurity Fundamentals",
+    description:
+      "An overview of essential cybersecurity practices to protect data and systems.",
+    video_url: "https://example.com/videos/cybersecurity-fundamentals",
+    type: "repeats",
+    muscles: ["chest", "traps"],
+  },
+  {
+    id: 14,
+    name: "Agile Project Management",
+    description:
+      "Understand the principles of Agile project management and how to apply them in real projects.",
+    video_url: "https://example.com/videos/agile-project-management",
+    type: "repeats",
+    muscles: ["chest", "traps"],
+  },
+  {
+    id: 15,
+    name: "Data Visualization with Tableau",
+    description:
+      "Learn how to create compelling data visualizations using Tableau.",
+    video_url: "https://example.com/videos/data-visualization-with-tableau",
     type: "repeats",
     muscles: ["chest", "traps"],
   },
@@ -126,10 +177,18 @@ export function CreateWorkoutForm() {
   });
 
   const onSubmit = async (values: createWorkoutType) => {
+    const body = {
+      ...values,
+      exercises: values.exercises.map((exercise) => {
+        return { ...exercise, time: parseInt(exercise.time) };
+      }),
+    };
+    console.log(body);
     // const data = await mutateAsync();
     // if (data.error) return;
     // form.reset();
   };
+
   return (
     <>
       <Form {...form}>
@@ -185,9 +244,11 @@ export function CreateWorkoutForm() {
                                     );
                                     field.onChange([...current]);
                                   } else {
-                                    field.onChange([...field.value, exercise]);
+                                    field.onChange([
+                                      ...field.value,
+                                      { ...exercise, time: "" },
+                                    ]);
                                   }
-                                  // console.log(form.getValues("exercises"));
                                 }}
                               >
                                 {exercise.name}
@@ -204,66 +265,83 @@ export function CreateWorkoutForm() {
                         </CommandGroup>
                       </CommandList>
                     </Command>
+                    <TooltipProvider>
+                      <div className="space-y-1 pt-2">
+                        {field.value.map((exercise, index) => {
+                          return (
+                            <Tooltip key={index}>
+                              <div className="flex items-center gap-2 rounded-xl border-2 bg-popover px-4 py-2">
+                                <FormField
+                                  control={form.control}
+                                  name={`exercises.${index}.time` as const}
+                                  render={({ field }) => (
+                                    <FormItem>
+                                      <FormControl>
+                                        <div className="relative flex-1">
+                                          <Input
+                                            {...field}
+                                            className="min-w-24 pl-11"
+                                            type="number"
+                                            inputMode="numeric"
+                                            min={1}
+                                            max={300}
+                                          />
+                                          <TooltipTrigger asChild>
+                                            <Button
+                                              size={"icon"}
+                                              variant={"secondary"}
+                                              className="absolute bottom-0 left-0 text-lg"
+                                            >
+                                              {exercise.type === "repeats" ? (
+                                                <TiArrowRepeatOutline
+                                                  size={26}
+                                                />
+                                              ) : (
+                                                <FaClock size={26} />
+                                              )}
+                                            </Button>
+                                          </TooltipTrigger>
+                                          <TooltipContent
+                                            className="font-mono text-lg font-semibold uppercase"
+                                            side="left"
+                                          >
+                                            {exercise.type}
+                                          </TooltipContent>
+                                        </div>
+                                      </FormControl>
+                                    </FormItem>
+                                  )}
+                                />
+                                <Badge
+                                  variant={"secondary"}
+                                  className="flex-1 items-center justify-between font-mono text-lg uppercase"
+                                >
+                                  <span className="flex-1">
+                                    {exercise.name}
+                                  </span>
+                                  <IoIosCloseCircle
+                                    size={24}
+                                    className="cursor-pointer transition-all hover:brightness-75"
+                                    onClick={() => {
+                                      const current = field.value.filter(
+                                        (key) => key.id !== exercise.id,
+                                      );
+                                      field.onChange([...current]);
+                                    }}
+                                  />
+                                </Badge>
+                              </div>
+                            </Tooltip>
+                          );
+                        })}
+                      </div>
+                    </TooltipProvider>
                   </>
                 </FormControl>
                 <FormMessage className="text-base" />
               </FormItem>
             )}
           />
-          {/* {form.getValues("exercise_time").map((_, index) => (
-            <div className="space-y-1 pt-2" key={index}>
-              {field.value.length > 0 &&
-                field.value.map((exercise, index) => {
-                  const current = field.value.map((_exercise) => {
-                    return { id: _exercise.id };
-                  });
-                  console.log(form.getValues("exercise_time"));
-                  return (
-                    <div key={index} className="flex items-center gap-1">
-                      <div className="relative flex-1">
-                        <Input
-                          type="number"
-                          className={`flex-1 text-base ${exercise.type === "repeats" ? "pl-12" : "pl-24"}`}
-                          placeholder={
-                            exercise.type === "repeats" ? "16" : "00:20"
-                          }
-                          onChange={(e) => {
-                            form.setValue("exercise_time", [...current, {}]);
-                            return {
-                              id: exercise.id,
-                              time: e.target.value,
-                            };
-                          }}
-                        />
-                        <Button
-                          variant={"secondary"}
-                          className="absolute bottom-0 left-0"
-                        >
-                          {exercise.type === "repeats" ? "x" : "seconds"}
-                        </Button>
-                      </div>
-
-                      <Badge
-                        variant={"secondary"}
-                        className="flex-1 cursor-default select-none justify-between font-mono text-base uppercase"
-                      >
-                        <span>{exercise.name}</span>
-                        <IoIosCloseCircle
-                          size={24}
-                          className="cursor-pointer transition-all hover:brightness-75"
-                          onClick={() => {
-                            const current = field.value.filter(
-                              (key) => key !== exercise,
-                            );
-                            field.onChange([...current]);
-                          }}
-                        />
-                      </Badge>
-                    </div>
-                  );
-                })}
-            </div>
-          ))} */}
 
           <Button
             disabled={isPending}
--- src/schemas/create-workout-schema.ts
+++ src/schemas/create-workout-schema.ts
@@ -7,16 +7,12 @@ const exercise = z.object({
   video_url: z.string(),
   type: z.string(),
   muscles: z.array(z.string()),
-});
-const exercise_time = z.object({
-  id: z.number(),
-  time: z.string(),
+  time: z.string().min(1),
 });
 
 export const createWorkoutSchema = z.object({
   name: z.string().min(2).max(50),
   exercises: z.array(exercise),
-  exercise_time: z.array(exercise_time),
 });
 
 export type createWorkoutType = z.infer<typeof createWorkoutSchema>;
@@ -24,5 +20,4 @@ export type createWorkoutType = z.infer<typeof createWorkoutSchema>;
 export const createWorkoutDefault = {
   name: "",
   exercises: [],
-  exercise_time: [],
 };
